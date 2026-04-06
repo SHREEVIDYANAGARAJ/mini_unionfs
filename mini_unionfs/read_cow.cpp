a816463f9c5cd3366870db9778a32d7ff92e33ae
ShreyaA2105 <pes2ug23cs559@pesu.pes.edu>
2026-04-06 22:20:55 +0530

    updated read_cow file

--- mini_unionfs/read_cow.cpp
+++ mini_unionfs/read_cow.cpp
@@ -4,24 +4,45 @@
 static int unionfs_open(const char *path,
                         struct fuse_file_info *fi) {
     (void)path; (void)fi;
-          
-    return -ENOENT;
+
+
+    char resolved[PATH_MAX];
+    bool is_upper;
+
+    int ret = resolve_path(path, resolved, &is_upper);
+    if (ret < 0) return ret;
+
+    int fd = open(resolved, fi->flags);
+    if (fd == -1) return -errno;
+
+    close(fd);
+    return 0;
 }
 
 static int unionfs_read(const char *path, char *buf,
                         size_t size, off_t offset,
                         struct fuse_file_info *fi) {
-    (void)path; (void)buf;
-    (void)size; (void)offset; (void)fi;
+    (void)fi;
 
+    char resolved[PATH_MAX];
+    bool is_upper;
 
-    return -ENOENT;
+    int ret = resolve_path(path, resolved, &is_upper);
+    if (ret < 0) return ret;
+
+    int fd = open(resolved, O_RDONLY);
+    if (fd == -1) return -errno;
+
+    ssize_t res = pread(fd, buf, size, offset);
+    close(fd);
+
+    if (res == -1) return -errno;
+    return (int)res;
 }
 
 static int unionfs_release(const char *path,
                             struct fuse_file_info *fi) {
     (void)path;
-
     if (fi->fh) {
         close((int)fi->fh);
         fi->fh = 0;
